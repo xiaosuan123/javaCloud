e99c1e0faf013ad24d384bc267d55b0c4e89b432
王绍羽 <11073105+shaoyu1007@user.noreply.gitee.com>
2024-08-08 22:59:07 +0800

    完成负载均衡

--- order-service/src/main/java/cn/itcast/order/OrderApplication.java
+++ order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -3,7 +3,9 @@ package cn.itcast.order;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @MapperScan("cn.itcast.order.mapper")
 @SpringBootApplication
@@ -11,6 +13,17 @@ public class OrderApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OrderApplication.class, args);
+    }
+
+    /**
+     * 创建并配置RestTemplate bean
+     *
+     * @return 配置了负载均衡的RestTemplate实例
+     */
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
 }
--- order-service/src/main/java/cn/itcast/order/service/OrderService.java
+++ order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -2,19 +2,38 @@ package cn.itcast.order.service;
 
 import cn.itcast.order.mapper.OrderMapper;
 import cn.itcast.order.pojo.Order;
+import cn.itcast.order.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class OrderService {
 
     @Autowired
     private OrderMapper orderMapper;
+    @Autowired
+    private RestTemplate restTemplate;
 
+    /**
+     * 根据订单ID查询订单详情
+     * 此方法不仅查询订单信息，还通过调用用户服务获取用户信息，并将其关联到订单中
+     *
+     * @param orderId 订单ID，用于标识要查询的订单
+     * @return 包含用户信息的订单对象如果订单或用户不存在，则返回null
+     */
     public Order queryOrderById(Long orderId) {
-        // 1.查询订单
+        // 通过订单ID查询订单基本信息
         Order order = orderMapper.findById(orderId);
-        // 4.返回
+        // 构建查询用户信息的URL，根据订单中的用户ID
+        String url = "http://orderservice/user/" + order.getUserId();
+
+        // 调用用户服务，查询用户信息，并将其设置到订单对象中
+        User user = restTemplate.getForObject(url, User.class);
+        order.setUser(user);
+
+        // 返回包含用户信息的订单对象
         return order;
     }
+
 }
